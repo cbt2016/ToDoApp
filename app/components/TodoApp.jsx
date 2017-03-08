061083d52bcd2d329887741f93ce260f701d4351
Simo hans <simo@gmail.com>
2017-03-09 00:52:44 +0100

    Passing todos to another component TodoList

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,25 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function(){
+    return ({
+      todos:[
+        {
+          id:1,
+          text: 'Watch soccer'
+        },
+        {
+          id: 2,
+          text: 'working out '
+        }
+      ]
+    });
+  },
   render: function(){
+    var {todos} = this.state;
     return (
-      <p>Todo App </p>
+      <TodoList todos={todos}/>
     );
   }
 });
