1241da1d5c87248bcd0c79464b77a47edb391f47
Simo hans <simo@gmail.com>
2017-03-09 00:10:26 +0100

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,23 @@
+//Create a reactjs component
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+
+//load foundation
+require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
+$(document).foundation();
+
+//app css
+require('style-loader!css-loader!sass-loader!app.scss');
+ReactDOM.render(
+    // <Router history ={hashHistory}>
+    //   <Route path='/' component={Main}>
+    //     <Route path="countdown" component={Countdown}/>
+    //     <IndexRoute component={Timer}/>
+    //   </Route>
+    // </Router>,
+    <p>Boilerplate React </p>,
+    document.getElementById('app')
+);
